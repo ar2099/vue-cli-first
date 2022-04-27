d4566d1fdbeae596114cdf223f4560597ccf566c
Andrea <onemanshow2099@gmail.com>
2022-04-27 15:42:02 +0200

    finito

---
+++ progetto/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <TitoloAdesso></TitoloAdesso>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import TitoloAdesso from './components/titoloAdesso.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TitoloAdesso
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ progetto/src/components/titoloAdesso.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+<h2>Titolo componente</h2>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'TitoloAdesso',
+  
+  
+}
+</script>
+
+<style scoped>
+h2 {
+    font-size: 2em;
+    color: red;
+}
+</style>
